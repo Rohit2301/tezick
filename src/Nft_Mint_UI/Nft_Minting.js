4b7cae806eff0185fb5a71910211dfd6dcc52244
Rohit Duhan <rohitduhan2301@gmail.com>
2022-03-26 08:53:49 +0530

    NftIpfs boiler plate code

--- src/Nft_Mint_UI/Nft_Minting.js
+++ src/Nft_Mint_UI/Nft_Minting.js
@@ -1,7 +1,10 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { NFTStorage, File } from "nft.storage";
-
+import db from "../fireabse";
+import config from "../config/config";
+import html2canvas from "html2canvas";
+import firebase from "firebase";
 
 
 
@@ -9,3 +12,98 @@ const client = new NFTStorage({
   token:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFDQmEwMDVCYTM5YTVhMzEwQzVCNDk5RTQ1NWNGNkY4QzJmMjA0YjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODI2MDEwNTM0MywibmFtZSI6IlRlenRpY2sifQ.7csyuIYzuN22lTH2wWoqT1OYFwxivBsXsNujRFfYy-E",
 });
+
+const NftMinting = () => {
+  const printRef = React.useRef();
+  const { account, web3 } = useMoralis();
+
+  const handleDownloadImage = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const contract = new web3.eth.Contract(nftABI, config.nftContractAddress);
+    const data = canvas.toBlob(
+      async (blob) => {
+        let file = new File([blob], "ChessPosNft.png", { type: "image/png" });
+        const metadata = await client.store({
+          name: "chessPos position NFT",
+          description: description,
+          image: file,
+        });
+        console.log("metadata:", metadata.ipnft);
+        try {
+          const createCall = contract.methods
+            .createToken(metadata.url, chessGame1.fen())
+            .send({
+              from: account,
+            });
+
+          createCall.on("transactionHash", (hash) => {
+            console.log(hash);
+          });
+
+          createCall.on("receipt", (recipt) => {
+            console.log("reciept:", recipt);
+            let tokenId = parseInt(recipt.events.Transfer.returnValues.tokenId);
+            console.log("tokenId:", tokenId);
+            firebase
+              .storage()
+              .ref(`nfts/${metadata.ipnft}`)
+              .put(blob)
+              .then((image) => {
+                image.ref.getDownloadURL().then((url) => {
+                  console.log(url);
+                  db.collection("nfts")
+                    .doc(metadata.ipnft)
+                    .set({
+                      tokenId: tokenId,
+                      file: url,
+                      uri: chessGame1.fen(),
+                      metadataUri: metadata.ipnft,
+                      white: game.white.toLowerCase(),
+                      black: game.black.toLowerCase(),
+                      amountAtStake: game.amount,
+                      createdAt: new Date(),
+                      winner: game.winner,
+                      txnHash: recipt.transactionHash,
+                      creator: account.toLowerCase(),
+                      owner: account.toLowerCase(),
+                      type: "position",
+                    })
+                    .then(() => {
+                      alert("Your position's NFT is minted successfully.");
+                      navigate("/single-nft", {
+                        state: {
+                          nft: {
+                            tokenId: tokenId,
+                            file: url,
+                            uri: chessGame1.fen(),
+                            metadataUri: metadata.ipnft,
+                            amountAtStake: game.amount,
+                            createdAt: new Date(),
+                            txnHash: recipt.transactionHash,
+                            creator: account,
+                            owner: account,
+                            type: "position",
+                          },
+                        },
+                      });
+                    });
+                });
+              });
+          });
+
+          createCall.on("error", (error, recipt) => {
+            console.log(error);
+            alert(error);
+          });
+        } catch (error) {
+          alert(error.message);
+        }
+      },
+      "image/png",
+      1
+    );
+  };
+};
+
+export default NftMinting;
